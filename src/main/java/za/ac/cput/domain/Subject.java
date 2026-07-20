a05dcf4b916741b39df91eadea3d73c186aa1fda
RK Stock <230875807@mycput.ac.za>
2026-07-20 17:15:32 +0200

    Implemented Subject.java builder

--- src/main/java/za/ac/cput/domain/Subject.java
+++ src/main/java/za/ac/cput/domain/Subject.java
@@ -1,10 +1,93 @@
 package za.ac.cput.domain;
 
-public class Subject {
-    private long subjectId;
+// SubjectClass.java
+//Subject model class
+//Author:Reotshepile Stock (230875807)
+//Date:17 March 2026
 
-    public long getSubjectId() {
+public class Subject {
+
+    private Long subjectId;
+    private String subjectName;
+    private String subjectCode;
+    private String department;
+
+    public Subject(){
+        
+    }
+
+    private Subject(Builder builder) {
+        this.subjectId = builder.subjectId;
+        this.subjectName = builder.subjectName;
+        this.subjectCode = builder.subjectCode;
+        this.department = builder.department;
+    }
+
+    public Long getSubjectId() {
         return subjectId;
     }
 
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "subjectId=" + subjectId +
+                "department=" + department +
+                ", subjectName='" + subjectName + '\'' +
+                ", subjectCode='" + subjectCode + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+
+        private Long subjectId;
+        private String subjectName;
+        private String subjectCode;
+        private String department;
+
+        public Builder setSubjectId(Long subjectId) {
+            this.subjectId = subjectId;
+            return this;
+        }
+
+        public Builder setSubjectName(String subjectName) {
+            this.subjectName = subjectName;
+            return this;
+        }
+
+        public Builder setDepartment(String department) {
+            this.department = department;
+            return this;
+        }
+
+        public Builder setSubjectCoe(String subjectCode){
+            this.subjectCode = subjectCode;
+            return this;
+
+        }
+
+        public Builder copy(Subject subject) {
+            this.subjectId = subject.subjectId;
+            this.subjectName = subject.subjectName;
+            this.subjectCode = subject.subjectCode;
+            this.department = subject.department;
+
+            return this;
+        }
+
+        public Subject build() {
+            return new Subject(this);
+        }
+    }
 }
