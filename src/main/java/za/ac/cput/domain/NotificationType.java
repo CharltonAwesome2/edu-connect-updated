712c52b8a3782c6aba2a69cbbaa1ccbb174469f0
RISTechnologies01 <220118876@mycput.ac.za>
2026-07-26 15:56:38 +0200

    Add NotificationType enum for notification types

---
+++ src/main/java/za/ac/cput/domain/NotificationType.java
@@ -0,0 +1,11 @@
+package za.ac.cput.domain;
+
+public enum NotificationType {
+
+    BOOKING_CONFIRMATION,
+    BOOKING_REMINDER,
+    SESSION_UPDATE,
+    PAYMENT_CONFIRMATION,
+    REVIEW_RECEIVED,
+    SYSTEM_ALERT
+}
