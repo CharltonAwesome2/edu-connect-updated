a32816379181e35a001c18fc474bfaabaa1a60b5
CharltonAwesome2 <220483418@mycput.ac.za>
2026-07-30 13:32:57 +0200

    Move files for 220118876 to one branch

---
+++ src/main/java/za/ac/cput/domain/Notification.java
@@ -0,0 +1,104 @@
+package za.ac.cput.domain;
+
+import java.time.LocalDateTime;
+
+import za.ac.cput.enums.NotificationType;
+
+public class Notification {
+
+    private Long notificationId;
+    private Long userId;
+    private String title;
+    private String message;
+    private NotificationType type;
+    private boolean isRead;
+    private LocalDateTime readAt;
+
+    private Notification(Builder builder) {
+        this.notificationId = builder.notificationId;
+        this.userId = builder.userId;
+        this.title = builder.title;
+        this.message = builder.message;
+        this.type = builder.type;
+        this.isRead = builder.isRead;
+        this.readAt = builder.readAt;
+    }
+
+    public Long getNotificationId() {
+        return notificationId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public NotificationType getType() {
+        return type;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public LocalDateTime getReadAt() {
+        return readAt;
+    }
+
+    public static class Builder {
+
+        private Long notificationId;
+        private Long userId;
+        private String title;
+        private String message;
+        private NotificationType type;
+        private boolean isRead;
+        private LocalDateTime readAt;
+
+        public Builder setNotificationId(Long notificationId) {
+            this.notificationId = notificationId;
+            return this;
+        }
+
+        public Builder setUserId(Long userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setMessage(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder setType(NotificationType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setRead(boolean read) {
+            isRead = read;
+            return this;
+        }
+
+        public Builder setReadAt(LocalDateTime readAt) {
+            this.readAt = readAt;
+            return this;
+        }
+
+        public Notification build() {
+            return new Notification(this);
+        }
+    }
+}
